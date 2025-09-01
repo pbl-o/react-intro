6ec564071237c0e056115b3f946a5123c4fa2a3c
pblo <pdiazarenas541@gmail.com>
2025-09-01 03:25:32 +0100

    FIX: typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-dark text-light text-center py-3">
-      <h4>&copy; Pizzería Ticcinio - Todos los derechos reservados</h4>
+      <h4>&copy; Pizzería Ticcino - Todos los derechos reservados</h4>
     </div>
   );
 };
